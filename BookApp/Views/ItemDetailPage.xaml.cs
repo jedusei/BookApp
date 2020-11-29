13c79fec07c4b70a3cbffc567564b89ac517ba41
Joseph Edusei <jedusei99@gmail.com>
2020-11-29 06:42:16 +0000

    Add project files.

---
+++ BookApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using BookApp.ViewModels;
+
+namespace BookApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
