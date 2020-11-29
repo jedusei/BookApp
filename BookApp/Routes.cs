1192244ff13d812db62bbaecc306f0434be41941
Joseph Edusei <jedusei99@gmail.com>
2020-11-29 10:05:19 +0000

    Implemented intro page functionality.

---
+++ BookApp/Routes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookApp
+{
+    public static class Routes
+    {
+        public const string LOGIN = "//Login";
+    }
+}
--- BookApp/ViewModels/IntroViewModel.cs
+++ BookApp/ViewModels/IntroViewModel.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Windows.Input;
-using Xamarin.Essentials;
+﻿using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace BookApp.ViewModels
 {
     public class IntroViewModel : BaseViewModel
     {
+        readonly int _pageCount = 3;
+        int _currentPage;
+
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set => SetProperty(ref _currentPage, value);
+        }
+        public ICommand NextCommand { get; private set; }
+        public ICommand SkipCommand { get; private set; }
+
         public IntroViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+            SkipCommand = new Command(() => CurrentPage = _pageCount - 1);
+            NextCommand = new Command(async () =>
+            {
+                if (_currentPage < _pageCount - 1)
+                    CurrentPage++;
+                else
+                    await Shell.Current.GoToAsync(Routes.LOGIN);
+            });
         }
-
-        public ICommand OpenWebCommand { get; }
     }
 }
