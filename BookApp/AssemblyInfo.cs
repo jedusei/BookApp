abf9036470d3db26d8f8b5439e878481481fcb83
Joseph Edusei <jedusei99@gmail.com>
2020-11-30 08:05:32 +0000

    Added Syncfusion TabView on main page.

--- BookApp/AssemblyInfo.cs
+++ BookApp/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Circular-Std.ttf", Alias = "CircularStd")]
+[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "MaterialIcons")]
