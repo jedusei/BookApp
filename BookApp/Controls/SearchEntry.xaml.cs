7f21c23b3268b3370ad34f2fc6e3a857bb187ec4
Joseph Edusei <jedusei99@gmail.com>
2020-11-30 11:01:58 +0000

    Added SearchEntry control.

---
+++ BookApp/Controls/SearchEntry.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BookApp.Controls
+{
+    [System.ComponentModel.DesignTimeVisible(true)]
+    public partial class SearchEntry : Frame
+    {
+        public static BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(FormEntry), defaultBindingMode: BindingMode.TwoWay);
+        public static BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(FormEntry));
+        public static BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(ImageSource), typeof(FormEntry));
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+        public ImageSource Icon
+        {
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public SearchEntry()
+        {
+            InitializeComponent();
+        } 
+    }
+}
