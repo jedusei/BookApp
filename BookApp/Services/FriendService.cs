835cd037d5a621400d38c2f7926a5a7144c778eb
Joseph Edusei <jedusei99@gmail.com>
2020-12-02 20:49:15 +0000

    Designed friends list tab.

--- BookApp/Services/FriendService.cs
+++ BookApp/Services/FriendService.cs
@@ -1,5 +1,6 @@
 ﻿using BookApp.Models;
 using MvvmHelpers;
+using System;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,7 +10,30 @@ namespace BookApp.Services
 {
     class FriendService : IFriendService
     {
-        ObservableRangeCollection<Friend> _friends = new ObservableRangeCollection<Friend>();
+        ObservableRangeCollection<Friend> _friends = new ObservableRangeCollection<Friend>(new Friend[] {
+            new Friend
+            {
+                Name = "Rachel",
+                ProfileImageUrl = "https://dieteticallyspeaking.com/wp-content/uploads/2017/01/profile-square.jpg",
+                Messages = new ObservableCollection<Message>(new Message[]
+                {
+                    new Message
+                    {
+                        Content = "Quisque dictum varius arcu, eu scelerisque arcu consectetur ut. Duis dapibus nulla vitae ipsum mattis porttitor eget suscipit tellus.",
+                        DateCreated = new DateTime(2020,12,2,13,30,0)
+                    }
+                })
+            },
+        });
+
+        public FriendService()
+        {
+            _friends.Add(_friends[0]);
+            _friends.Add(_friends[0]);
+            _friends.Add(_friends[0]);
+            _friends.Add(_friends[0]);
+            _friends.Add(_friends[0]);
+        }
 
         public async Task AddFriendsAsync(params Friend[] friends)
         {
