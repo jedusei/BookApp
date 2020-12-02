f3c3b537ccd222d353d19a4925169be4c2a19afb
Joseph Edusei <jedusei99@gmail.com>
2020-12-02 17:08:09 +0000

    Added ripple effect to book list item.

--- BookApp.iOS/AppDelegate.cs
+++ BookApp.iOS/AppDelegate.cs
@@ -24,7 +24,10 @@ namespace BookApp.iOS
         {
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental", "Brush_Experimental", "Shapes_Experimental", "CarouselView_Experimental");
             global::Xamarin.Forms.Forms.Init();
+            Syncfusion.XForms.iOS.EffectsView.SfEffectsViewRenderer.Init();
+
             LoadApplication(new App());
+
             Syncfusion.SfRating.XForms.iOS.SfRatingRenderer.Init();
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
 
