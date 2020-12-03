677506713725c9132f53a45fbe9d50e0af85eab9
Joseph Edusei <jedusei99@gmail.com>
2020-12-03 05:23:42 +0000

    Added bold font.

--- BookApp/AssemblyInfo.cs
+++ BookApp/AssemblyInfo.cs
@@ -3,4 +3,5 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Circular-Std.ttf", Alias = "CircularStd")]
+[assembly: ExportFont("Circular Std Bold.ttf", Alias = "CircularStdBold")]
 [assembly: ExportFont("materialdesignicons-webfont.ttf", Alias = "MaterialIcons")]
