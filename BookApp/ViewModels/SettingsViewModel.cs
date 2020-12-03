3cd5001d0f8a59025048af77625f99dc9f13573a
Joseph Edusei <jedusei99@gmail.com>
2020-12-03 14:11:57 +0000

    Designed Settings tab.

---
+++ BookApp/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace BookApp.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        public ICommand OpenSectionCommand { get; private set; }
+    }
+}
---
+++ BookApp/Views/SettingsTab.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BookApp.ViewModels;
+using BookApp.Views.Base;
+using Xamarin.Forms;
+
+namespace BookApp.Views
+{
+    public partial class SettingsTab : ContentView, ITabContentView
+    {
+        SettingsViewModel _viewModel;
+
+        public SettingsTab()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new SettingsViewModel();
+        }
+
+        public void OnStart()
+        {
+            _viewModel.OnStart();
+        }
+    }
+}
